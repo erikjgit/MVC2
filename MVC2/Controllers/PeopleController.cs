ebd3806e45cbb0b4e5b84c49111a72281ac84b42
Erik Johannesen <erikjj@protonmail.com>
2021-10-25 13:32:45 +0200

    Removed some things that were just there to test

--- MVC2/Controllers/PeopleController.cs
+++ MVC2/Controllers/PeopleController.cs
@@ -39,13 +39,6 @@ namespace MVC2.Controllers
             createPersonViewModel.Name = peopleViewModel.Name;
             createPersonViewModel.City = peopleViewModel.City;
             createPersonViewModel.Phone = peopleViewModel.Phone;
-            //createPersonViewModel.Phone = 2;            
-            //PropertyInfo pi = createPersonViewModel.GetType().GetProperty("Phone");
-            //int current = System.Convert.ToInt32(pi.GetValue(createPersonViewModel));
-            //current++;
-            //pi.SetValue(createPersonViewModel, current);
-            //createPersonViewModel.GetType().GetProperty("Phone").SetValue(createPersonViewModel, createPersonViewModel);
-            //createPersonViewModel.Phone = (createPersonViewModel.GetType().GetProperty("Phone").
             _peopleService.Add(createPersonViewModel);
             return RedirectToAction("Index", peopleViewModel);
         }
