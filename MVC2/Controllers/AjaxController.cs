64feaf1e2ed649794e3dcdcaa9722bfa3a7a6c8a
Erik Johannesen <erikjj@protonmail.com>
2021-11-19 23:54:41 +0100

    Changed AjaxController so it works when Person has a City object instead of a string.

--- MVC2/Controllers/AjaxController.cs
+++ MVC2/Controllers/AjaxController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC2.Models;
 using MVC2.Models.Repo;
 using MVC2.Models.Service;
+using MVC2.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +14,16 @@ namespace MVC2.Controllers
     {
         IPeopleRepo _inMemoryPeopleRepo;
         IPeopleService _peopleService;
-        public AjaxController (IPeopleRepo inMemoryPeopleRepo, IPeopleService peopleService)
+        ICityService _cityService;
+        IPersonLanguageService _personLanguageService;
+        ILanguageService _languageService;
+        public AjaxController (IPeopleRepo inMemoryPeopleRepo, IPeopleService peopleService, ICityService cityService, IPersonLanguageService personLanguageService, ILanguageService languageService)
         {
             _inMemoryPeopleRepo = inMemoryPeopleRepo;
             _peopleService = peopleService;
+            _cityService = cityService;
+            _personLanguageService = personLanguageService;
+            _languageService = languageService;
         }
         public IActionResult Index()
         {
@@ -23,13 +31,40 @@ namespace MVC2.Controllers
         }
         public IActionResult People()
         {
+            PeopleViewModel vm = new PeopleViewModel();
+            vm = _peopleService.All();
+            foreach (Person p in vm.PersonList)
+            {
+                p.PersonLanguages = _personLanguageService.FindBy(p);
+                foreach (PersonLanguage pl in p.PersonLanguages)
+                {
+                    pl.Language = _languageService.FindBy(pl.LanguageId);
+                }
+            }
+            vm.Cities = _cityService.All().Cities;
 
-            return PartialView("_PeopleListPartialView", _peopleService.All().PersonList);
+            return PartialView("_PeopleListPartialView", vm.PersonList);
         }
         [HttpPost]
         public IActionResult Detail(int id)
         {
-            return PartialView("_PersonPartial", _peopleService.FindBy(id));
+            PeopleViewModel vm = new PeopleViewModel();
+            Person person = new Person();
+            vm = _peopleService.All();
+            vm.Cities = _cityService.All().Cities;            
+            foreach (Person p in vm.PersonList)
+            {
+                if(p.Id == id)
+                {
+                    person = p;
+                }
+            }
+            person.PersonLanguages = _personLanguageService.FindBy(person);
+            foreach (PersonLanguage pl in person.PersonLanguages)
+            {
+                pl.Language = _languageService.FindBy(pl.LanguageId);
+            }
+            return PartialView("_PersonPartial", person);
         }
         public IActionResult Delete(int id)
         {
