a4ce3b373cc06ffa6ac15b80e4abae7f514dfc5b
Erik Johannesen <erikjj@protonmail.com>
2021-11-22 23:04:29 +0100

    Makinf changes to see if it will let me scaffold identity

--- MVC2/Database/PeopleDbContext.cs
+++ MVC2/Database/PeopleDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MVC2.Models;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MVC2.Database
 {
-    public class PeopleDbContext : DbContext
+    public class PeopleDbContext : IdentityDbContext
     {
         public PeopleDbContext(DbContextOptions<PeopleDbContext> options) : base(options)
         {
